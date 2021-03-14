3ec224fa2427b5915080912e099f01c02258a474
LAURENT Louis <louis_laurent@aol.fr>
2021-03-14 15:31:17 +0100

    add compute_type for select function to compute

--- template_python_project/main.py
+++ template_python_project/main.py
@@ -1,7 +1,9 @@
 import click
+from .core import compute
 
 @click.command()
+@click.option('--compute-type', '-c', type=click.Choice(['ADD', 'MUL'], case_sensitive=True))
 @click.option('--first', '-f', required=True, help='First number to handle')
 @click.option('--second', '-s', required=True, help='Second number to handle')
-def main(first, second):
-    print('first: {}, second: {}'.format(first, second))
+def main(compute_type, first, second):
+    print('compute_type: {}, first: {}, second: {}'.format(compute_type, first, second))
