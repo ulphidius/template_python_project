2aeb3b29465d468cb532ba5d25c413e157c8e83e
LAURENT Louis <louis_laurent@aol.fr>
2021-03-14 17:20:49 +0100

    add github url

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ setup(
     name='template_python_project',
     version='0.1.0',
     author='ulphidius',
+    url='https://github.com/ulphidius/template_python_project',
     description='Template test for python bin',
     long_description=CONTENT_README,
     long_description_content_type='text/markdown',
