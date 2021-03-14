03e22248e2a179683d482c24c4849bd8f4d0590a
LAURENT Louis <louis_laurent@aol.fr>
2021-03-14 15:37:13 +0100

    add logic to main

--- template_python_project/main.py
+++ template_python_project/main.py
@@ -6,4 +6,8 @@ from .core import compute
 @click.option('--first', '-f', required=True, help='First number to handle')
 @click.option('--second', '-s', required=True, help='Second number to handle')
 def main(compute_type, first, second):
-    print('compute_type: {}, first: {}, second: {}'.format(compute_type, first, second))
+    if compute_type == 'ADD':
+        print(compute.add(int(first), int(second)))
+
+    elif compute_type == 'MUL':
+        print(compute.multiply(int(first), int(second)))
