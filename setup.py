83f1d7f84014bba2b3be6d92d9167c95c72e9d32
LAURENT Louis <louis_laurent@aol.fr>
2021-03-14 16:48:39 +0100

    add setup for prod install modify .gitignore for ignore build and dist folders

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+with open('README.md', 'r') as readme:
+    CONTENT_README = readme.read()
+
+setup(
+    name='template_python_project',
+    version='0.1.0',
+    author='ulphidius',
+    description='Template test for python bin',
+    long_description=CONTENT_README,
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    install_requires=['click'],
+    entry_points='''
+        [console_scripts]
+        template_python_project=template_python_project.main:main 
+    '''
+)
