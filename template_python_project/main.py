0af82b7339d87014e714bd35aed7ed7136d1809a
LAURENT Louis <louis_laurent@aol.fr>
2021-03-14 15:23:10 +0100

    add cli interface

--- template_python_project/main.py
+++ template_python_project/main.py
@@ -1,2 +1,7 @@
-def main():
-    print('hello world !')
+import click
+
+@click.command()
+@click.option('--first', '-f', required=True, help='First number to handle')
+@click.option('--second', '-s', required=True, help='Second number to handle')
+def main(first, second):
+    print('first: {}, second: {}'.format(first, second))
