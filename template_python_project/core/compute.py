1f52fbebff8cc51951eb0ac45a4adb1785a4ad41
LAURENT Louis <louis_laurent@aol.fr>
2021-04-02 10:14:50 +0200

    add verbose and quiet option for let the user handle the log level modify the default log level to warn

--- template_python_project/core/compute.py
+++ template_python_project/core/compute.py
@@ -3,4 +3,3 @@ def multiply(value1, value2):
 
 def add(value1, value2):
     return value1 + value2
- 
--- template_python_project/main.py
+++ template_python_project/main.py
@@ -1,7 +1,7 @@
 import click
 from .core import compute
 from .logging import logger
-from logging import INFO
+from logging import DEBUG, INFO, WARNING, ERROR
 
 LOGGER = None
 
@@ -10,11 +10,23 @@ LOGGER = None
 @click.option('--first', '-f', required=True, help='First number to handle')
 @click.option('--second', '-s', required=True, help='Second number to handle')
 @click.option('--log-path', '-l', default=None, help='Path where log files are stored')
+@click.option('--quiet', '-q', default=False, is_flag=True, help='Ignore the information notifications (Conflict with the verbose option)')
+@click.option('--verbose', '-v', count=True, help='The first verbose option activate the information notifications and the second verbose option activate the debug notifications (Conflict with the quiet option)')
 @click.version_option()
-def main(compute_type, first, second, log_path):
+def main(compute_type, first, second, log_path, quiet, verbose):
     global LOGGER
 
-    LOGGER = logger.get_logger(INFO, log_path)
+    if quiet and verbose > 0:
+        raise click.UsageError('The option quiet and verbose cannot be used together')
+
+    if quiet:
+        LOGGER = logger.get_logger(ERROR, log_path)
+    elif verbose == 1:
+        LOGGER = logger.get_logger(INFO, log_path)
+    elif verbose == 2:
+        LOGGER = logger.get_logger(DEBUG, log_path)
+    else:
+        LOGGER = logger.get_logger(WARNING, log_path)
 
     if compute_type == 'ADD':
         LOGGER.info('Add function call')
