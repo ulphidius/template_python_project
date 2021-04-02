36a3ab17bde09eece6463f67f6ca35bf34e51b4c
LAURENT Louis <louis_laurent@aol.fr>
2021-04-02 13:11:24 +0200

    modify verbose/quiet conflict exception for add click context

--- template_python_project/main.py
+++ template_python_project/main.py
@@ -6,9 +6,9 @@ from logging import DEBUG, INFO, WARNING, ERROR
 LOGGER = None
 
 @click.command()
-@click.option('--compute-type', '-c', type=click.Choice(['ADD', 'MUL'], case_sensitive=True))
-@click.option('--first', '-f', required=True, help='First number to handle')
-@click.option('--second', '-s', required=True, help='Second number to handle')
+@click.option('--compute-type', '-c', required=True, type=click.Choice(['ADD', 'MUL'], case_sensitive=True))
+@click.option('--first', '-f', required=True, type=int, help='First number to handle')
+@click.option('--second', '-s', required=True, type=int, help='Second number to handle')
 @click.option('--log-path', '-l', default=None, help='Path where log files are stored')
 @click.option('--quiet', '-q', default=False, is_flag=True, help='Ignore the information notifications (Conflict with the verbose option)')
 @click.option('--verbose', '-v', count=True, help='The first verbose option activate the information notifications and the second verbose option activate the debug notifications (Conflict with the quiet option)')
@@ -17,7 +17,7 @@ def main(compute_type, first, second, log_path, quiet, verbose):
     global LOGGER
 
     if quiet and verbose > 0:
-        raise click.UsageError('The option quiet and verbose cannot be used together')
+        raise click.UsageError('The option quiet and verbose cannot be used together', ctx=click.get_current_context())
 
     if quiet:
         LOGGER = logger.get_logger(ERROR, log_path)
